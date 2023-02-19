12f42174716402cc909fd497484bdfb66a09dd5d
vadim_budarin <budarin-vv@yandex.ru>
2023-02-19 19:42:52 +0300

    feat: add code generation for stats

--- babel.config.js
+++ babel.config.js
@@ -7,7 +7,11 @@ module.exports = {
             {
                 target: 'node',
                 unsafeTransformations: true,
-                useStatsServer: false,
+                // useStatsServer: false,
+                useStatsServer: {
+                    host: '127.0.0.1',
+                    port: 3000,
+                },
             },
         ],
 
